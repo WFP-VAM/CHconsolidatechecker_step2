250a3179c1e65f4abaf2946bc740bc68a284f555
olanderb <billyolander@gmail.com>
2021-10-21 09:32:09 +0000

    runoffline script

---
+++ runoffline.R
@@ -0,0 +1,7 @@
+#install these packages in line below if they are not already - to do that remove the # from line below and run
+#install.packages(c("tidyverse", "openxlsx","DT","shinyBS","shiny"))
+
+#this will run shiny app from github repository https://github.com/WFP-VAM/CHconsolidatechecker_step2 on local machine
+library(shiny)
+
+runUrl('https://github.com/WFP-VAM/CHconsolidatechecker_step2/archive/main.tar.gz')
